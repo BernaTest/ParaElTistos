47344d58d1e5ace098ba01602edf7d3fede530dd
BernaTest <zayold.1@gmail.com>
2021-08-05 14:46:28 -0500

    Hola mundo

--- Assets/Scenes/Scipts/HolaMundo.cs
+++ Assets/Scenes/Scipts/HolaMundo.cs
@@ -7,7 +7,7 @@ public class HolaMundo : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hola Mundo");
+        Debug.Log("Hola Mundo 1");
     }
 
     // Update is called once per frame
